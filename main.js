2211c05c71a937e6f8a77d99bfcd11df05f529ac
vietbuiminh <buiminhviet.2002@gmail.com>
2020-04-25 20:27:17 -0700

    Added other element

--- main.js
+++ main.js
@@ -2,11 +2,31 @@
 
 // Are Case-Sensitive
 
-let HelloWorld = true;
+// let HelloWorld = true;
 
-let a, b, c:
-a = 1;
-b = 2;
-c = 3;
+// let a, b, c:
+// a = 1;
+// b = 2;
+// c = 3;
 
-console.log(age);
+// console.log(age);
+
+//constants 
+// const pi = 3.14;
+// pi = 5; --> cannot do this
+// console.log(pi);
+
+//String
+let name = 'Viet';
+let age = 17; //number literal
+let loveCats = true; //Boolean literal
+let color = null; //Null
+//let list = undefined; //undefind
+let list; //Same as undefind
+
+console.log(list);
+// console.log(name);
+
+name += ' Bui'; //same as name = name + ....
+let message = name + ' "is" ' + age //can use double or single quotation mark
+console.log(message);
